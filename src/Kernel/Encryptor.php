6299c551a89f19f54aa71b191e3ed8319652939c
Alim Rahman <boyaq@otkur.biz>
2020-01-16 02:11:45 +0800

    added encryptor class

---
+++ src/Kernel/Encryptor.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace OtkurBiz\ByteDance\Kernel;
+
+use OtkurBiz\ByteDance\Kernel\Exceptions\RuntimeException;
+use OtkurBiz\ByteDance\Kernel\Support\AES;
+use Throwable;
+use function OtkurBiz\ByteDance\Kernel\Support\str_random;
+
+/**
+ * Class Encryptor.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class Encryptor
+{
+    const ERROR_INVALID_SIGNATURE = -40001; // Signature verification failed
+    const ERROR_CALC_SIGNATURE = -40003; // Calculating the signature failed
+    const ERROR_INVALID_AES_KEY = -40004; // Invalid AESKey
+    const ERROR_INVALID_APP_ID = -40005; // Check AppID failed
+    const ERROR_ENCRYPT_AES = -40006; // AES EncryptionInterface failed
+    const ERROR_DECRYPT_AES = -40007; // AES decryption failed
+    const ERROR_BASE64_ENCODE = -40009; // Base64 encoding failed
+    const ERROR_BASE64_DECODE = -40010; // Base64 decoding failed
+    const ILLEGAL_BUFFER = -41003; // Illegal buffer
+
+    /**
+     * App id.
+     *
+     * @var string
+     */
+    protected $appId;
+
+    /**
+     * App token.
+     *
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var string
+     */
+    protected $aesKey;
+
+    /**
+     * Block size.
+     *
+     * @var int
+     */
+    protected $blockSize = 32;
+
+    /**
+     * Constructor.
+     *
+     * @param string      $appId
+     * @param string|null $token
+     * @param string|null $aesKey
+     */
+    public function __construct(string $appId, string $token = null, string $aesKey = null)
+    {
+        $this->appId = $appId;
+        $this->token = $token;
+        $this->aesKey = base64_decode($aesKey.'=', true);
+    }
+
+    /**
+     * Get the app token.
+     *
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+
+
+
+    /**
+     * Get SHA1.
+     *
+     * @return string
+     *
+     * @throws self
+     */
+    public function signature(): string
+    {
+        $array = func_get_args();
+        sort($array, SORT_STRING);
+
+        return sha1(implode($array));
+    }
+
+    /**
+     * PKCS#7 pad.
+     *
+     * @param string $text
+     * @param int    $blockSize
+     *
+     * @return string
+     *
+     * @throws \OtkurBiz\ByteDance\Kernel\Exceptions\RuntimeException
+     */
+    public function pkcs7Pad(string $text, int $blockSize): string
+    {
+        if ($blockSize > 256) {
+            throw new RuntimeException('$blockSize may not be more than 256');
+        }
+        $padding = $blockSize - (strlen($text) % $blockSize);
+        $pattern = chr($padding);
+
+        return $text.str_repeat($pattern, $padding);
+    }
+
+    /**
+     * PKCS#7 unpad.
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    public function pkcs7Unpad(string $text): string
+    {
+        $pad = ord(substr($text, -1));
+        if ($pad < 1 || $pad > $this->blockSize) {
+            $pad = 0;
+        }
+
+        return substr($text, 0, (strlen($text) - $pad));
+    }
+}
